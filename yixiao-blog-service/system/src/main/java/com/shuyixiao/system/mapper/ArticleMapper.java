c1fd805461846f8ce4b39ed03116bf65b4716c20
Shujh <chinasjh2022@126.com>
2024-06-14 16:10:15 +0800

    :sparkles: 实现文章展示功能

---
+++ yixiao-blog-service/system/src/main/java/com/shuyixiao/system/mapper/ArticleMapper.java
@@ -0,0 +1,16 @@
+package com.shuyixiao.system.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.shuyixiao.dto.Article;
+import org.springframework.stereotype.Repository;
+
+/**
+ * ClassName ArticleMapper.java
+ * author 舒一笑
+ * version 1.0.0
+ * Description 文章数据存储
+ * createTime 2024年06月14日 12:24:00
+ */
+@Repository
+public interface ArticleMapper extends BaseMapper<Article> {
+}
